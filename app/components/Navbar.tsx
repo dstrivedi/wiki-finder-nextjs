8dbe0bfd180f54f46e5b0ca7a3a23ba44ee2843c
Drashti Trivedi <drashti1118@gmail.com>
2023-10-09 23:14:07 -0400

    heading changed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <div>
             <nav className='p-4 bg-slate-950 flex justify-between flex-col md:flex-row sticky drop-shadow-xl'>
-                <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0 hover:text-slate-200'><Link href="/">WikiRocket!</Link></h1>
+                <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0 hover:text-slate-200'><Link href="/">WikiFinder!</Link></h1>
                 <Search />
             </nav>
         </div>
